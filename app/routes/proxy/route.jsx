74a2382be3fbe7acf02fe71d35ce90ef310f2ab1
keigo9 <keigo.matsunari@gmail.com>
2023-09-20 20:06:39 +0900

    tmp/app-proxy

---
+++ app/routes/proxy/route.jsx
@@ -0,0 +1,42 @@
+// app/routes/**\/.ts
+import { authenticate } from '../../shopify.server';
+
+export async function loader({request}) {
+  console.log("app proxy loaded")
+
+  try {
+    //const proxy = await authenticate.public.appProxy(request);
+    const { admin } = await authenticate.public.appProxy(request);
+    //console.log("proxy =>",proxy);
+    //console.log("admin =>",admin);
+    //console.log("session =>",session);
+
+    const response = await admin.graphql(
+      `query order {
+        orders(first: 10) {
+          nodes {
+            id
+            name
+            totalPrice
+            lineItems(first: 10) {
+              nodes {
+                title
+                quantity
+                originalUnitPrice
+              }
+            }
+          }
+        }
+      }`
+    );
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch(error) {
+    console.log("error", error);
+    return null;
+  }
+}
